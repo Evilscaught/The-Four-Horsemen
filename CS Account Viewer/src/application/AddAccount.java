80df9e6006afe8815f663f893a5f462fb3fd3bad
Scott McKay <Evilscaught@users.noreply.github.com>
2017-10-18 20:21:53 -0600

    No longer possible to create account with blank text fields
    Text fields will now turn red if user tries to create account without entering information for one of the text fields

--- CS Account Viewer/src/application/AddAccount.java
+++ CS Account Viewer/src/application/AddAccount.java
@@ -8,17 +8,14 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class AddAccount {
-	
-	public AddAccount() {
-		
-	}
-	
-
-	public Scene getScene(Stage stage, InputOutput inout) {
+public class AddAccount 
+{
+	public Scene getScene(Stage stage, InputOutput IO) 
+	{
 		
 		
 		GridPane grid = new GridPane();
@@ -28,56 +25,75 @@ public class AddAccount {
 		grid.setHgap(5);
 		grid.setAlignment(Pos.TOP_LEFT);
 		
-		
-		Text firstnameText = new Text("First name:");
-		
-		Text lastnameText = new Text("Last name");
-		
-		Text emailText = new Text("Email Address:");
-		
-		Text userText =  new Text("Username:");
-		
+		Text firstnameText = new Text("First name:"), lastnameText = new Text("Last name");
+		Text emailText = new Text("Email Address:"),  userText =  new Text("Username:");
 		Text passText = new Text ("Password");
 		
-		TextField firstnameField = new TextField();
-		
-		TextField lastnameField = new TextField();
-		
-		TextField emailField = new TextField();
-		
-		TextField userField = new TextField();
-		
+		TextField firstnameField = new TextField(), lastnameField = new TextField();
+		TextField emailField = new TextField(), userField = new TextField();
 		TextField passField = new TextField();
 		
 		
 		Button exitButton = new Button();
 		exitButton.setText(" Exit ");
-		exitButton.setOnAction(new EventHandler<ActionEvent>() {
-
+		exitButton.setOnAction(new EventHandler<ActionEvent>() 
+		{
 			@Override
-			public void handle(ActionEvent arg0) {
+			public void handle(ActionEvent arg0) 
+			{
 				stage.close();
 			}
 		});
 		
 		Button saveButton = new Button();
 		saveButton.setText("    Save    ");
-		saveButton.setOnAction(new EventHandler<ActionEvent>() {
-
+		saveButton.setOnAction(new EventHandler<ActionEvent>() 
+		{
 			@Override
-			public void handle(ActionEvent arg0) {
-				inout.createAccount(firstnameField.getText(), lastnameField.getText(), userField.getText(), emailField.getText(), passField.getText());
-				
+			public void handle(ActionEvent arg0) 
+			{
+				//Make sure no text fields are empty
+				if (firstnameField.getText().isEmpty() || lastnameField.getText().isEmpty() || userField.getText().isEmpty() || emailField.getText().isEmpty() || passField.getText().isEmpty())
+				{
+					//Set text field background to red if empty:
+					if (firstnameField.getText().isEmpty())
+					{
+						firstnameField.setStyle("-fx-background-color: red;");
+					}
+					if (lastnameField.getText().isEmpty())
+					{
+						lastnameField.setStyle("-fx-background-color: red;");
+					}
+					if (userField.getText().isEmpty())
+					{
+						userField.setStyle("-fx-background-color: red;");
+					}
+					if (emailField.getText().isEmpty())
+					{
+						emailField.setStyle("-fx-background-color: red;");
+					}
+					if (passField.getText().isEmpty())
+					{
+						passField.setStyle("-fx-background-color: red;");
+					}
+				}
+				//Create account if no text fields are empty
+				else
+				{
+					IO.createAccount(firstnameField.getText(), lastnameField.getText(), userField.getText(), emailField.getText(), passField.getText());	
+				}
 			}
 		});
 		
 		Button backButton = new Button();
 		backButton.setText("  Back  ");
-		backButton.setOnAction(new EventHandler<ActionEvent>() {
+		backButton.setOnAction(new EventHandler<ActionEvent>() 
+		{
 
 			@Override
-			public void handle(ActionEvent arg0) {
-				Scene scene2 = new Menu2().getScene(stage, inout);
+			public void handle(ActionEvent arg0) 
+			{
+				Scene scene2 = new Menu2().getScene(stage, IO);
 				stage.setScene(scene2);
 			}
 		}
@@ -103,8 +119,6 @@ public class AddAccount {
 		
 
 		Scene scene = new Scene(grid);
-		return scene;
-		
+		return scene;	
 	}
-
 }
