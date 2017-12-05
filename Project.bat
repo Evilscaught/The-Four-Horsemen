2bdd979b9fdf4978b38171d9a5456b80e1c439dd
DanB28 <dan.bailey28@gmail.com>
2017-12-05 10:32:41 -0700

    Fixed bat file to run

--- Project.bat
+++ Project.bat
@@ -1,5 +1,6 @@
 @echo off
-cd CS Account Viewer\src\
-javac application\*.java
-java application.Main
+cd CS Account Viewer\src\application
+javac *.java controller\*.java -cp "..\..\Libraries\algs4.jar;stdlib.jar"; 
+cd..
+java -cp "..\Libraries\algs4.jar;stdlib.jar;" application.Main
 pause
